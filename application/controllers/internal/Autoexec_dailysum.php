cde8e56b1198811dc1ba539cfde9b88289f481d9
uplink42 <diamantinorgf@gmail.com>
2017-01-17 20:46:17 +0000

    Fixed

--- application/controllers/internal/Autoexec_dailysum.php
+++ application/controllers/internal/Autoexec_dailysum.php
@@ -1,7 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-ini_set('mysql.connect_timeout', '3000');
-ini_set('default_socket_timeout', '3000');
 ini_set('max_execution_time', '0');
 
 error_reporting(E_ALL);
